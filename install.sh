0d14cdf3c2c91f7f0928b90f594b8c400b976165
script47ph <script47ind@gmail.com>
2022-07-31 14:24:08 +0700

    Fix missing configuration

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo "Update and upgrade repository..."
 sudo apt-get update > /dev/null && sudo apt-get upgrade -y > /dev/null
 echo "Install dependencies..."
 sudo apt-get install -y git zsh wget unzip vim > /dev/null
+curl -sL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash > /dev/null
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' $HOME/.zshrc
 sudo wget -q https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir -p $HOME/.poshthemes
@@ -11,9 +14,9 @@ unzip $HOME/.poshthemes/themes.zip -d $HOME/.poshthemes > /dev/null
 chmod u+rw $HOME/.poshthemes/*.omp.*
 rm $HOME/.poshthemes/themes.zip
 echo "Setup zsh as default shell..."
-echo "eval '$(oh-my-posh init zsh --config ~/.poshthemes/material.omp.json)'" >> $HOME/.zshrc
+echo -e 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/material.omp.json)" \nalias="clear"' >> $HOME/.zshrc
 chsh -s $(which zsh)
-echo "set number" \n "syntax on" > $HOME/.vimrc
+echo -e "set number \nsyntax on" > $HOME/.vimrc
 echo "Successfully installed! Enjoy your new shell!" "Exiting..."
 sleep 3
 exit 0
